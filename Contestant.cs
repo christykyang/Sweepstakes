0d1bb4a73cdc610623a65c7c5ac5a5bd07b7000c
unknown <christykyang@gmail.com>
2020-03-03 11:12:00 -0600

    Looks like we finished the Contestant class and used the static UserInterface class. I feel like it's reduntant but I could be wrong, fo now it works, thus leave it until you reach MVP.

--- Contestant.cs
+++ Contestant.cs
@@ -9,13 +9,43 @@ namespace SweepstakesProject
     public class Contestant
     {
         //member variabless (HAS A)
-        string firstName;
-        string lastName;
-        string emailAddress;
-        int registrationNumber;
+        string firstName = "";
+        string lastName = "";
+        string emailAddress = "";
+        int registrationNumber = 0;
 
         //constructor (SPAWNER)
+        public Contestant()
+        {
+            GetFirstName();
+            GetLastName();
+            GetEmail();
+            GenerateNumber();
+        }
 
         //member methods (CAN DO)
+        public void GetFirstName()
+        {
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+            UserInterface.GetUserInputFor(firstName);
+        }
+        public void GetLastName()
+        {
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+            UserInterface.GetUserInputFor(lastName);
+        }
+        public void GetEmail()
+        {
+            Console.WriteLine("What is your email address?");
+            emailAddress = Console.ReadLine();
+            UserInterface.GetUserInputFor(emailAddress);
+        }
+        public void GenerateNumber()
+        {
+            registrationNumber++;
+        }
     }
+
 }
